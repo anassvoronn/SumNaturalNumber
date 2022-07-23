47bbb96a89695016ff631aef27617de8e9f4231a
anassvoronn <UQzbwQCt7R3uMfz>
2022-07-23 15:43:15 +0300

    sum of first 10 natural number

---
+++ src/main/java/org/dz/solution/SumNaturalNumber.java
@@ -0,0 +1,4 @@
+package org.dz.solution;
+
+public class SumNaturalNumber {
+}
---
+++ src/test/java/org/dz/solution/SumNaturalNumberTest.java
@@ -0,0 +1,19 @@
+package org.dz.solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SumNaturalNumberTest {
+
+    @Test
+    public void task3() {
+        int sum = 0;
+        for (int i = 1; i <= 1; i++) {
+            sum = 1+2+3+4+5+6+7+8+9+10;
+            System.out.println(sum);
+
+        }
+    }
+
+}
