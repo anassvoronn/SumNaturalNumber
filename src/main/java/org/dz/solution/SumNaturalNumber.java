8967f5afeacb56930cd06a6a0bac05f88d463fbf
anassvoronn <UQzbwQCt7R3uMfz>
2022-07-24 14:31:57 +0300

    sum of first 10 natural number

--- src/main/java/org/dz/solution/SumNaturalNumber.java
+++ src/main/java/org/dz/solution/SumNaturalNumber.java
@@ -1,4 +1,14 @@
 package org.dz.solution;
 
 public class SumNaturalNumber {
+    public int getSumNaturalNumbersFrom1ToN(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("Number can't be negative");
+        }
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum = i + sum;
+        }
+        return sum;
+    }
 }
--- src/test/java/org/dz/solution/SumNaturalNumberTest.java
+++ src/test/java/org/dz/solution/SumNaturalNumberTest.java
@@ -1,5 +1,6 @@
 package org.dz.solution;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,13 +8,37 @@ import static org.junit.Assert.*;
 public class SumNaturalNumberTest {
 
     @Test
-    public void task3() {
-        int sum = 0;
-        for (int i = 1; i <= 1; i++) {
-            sum = 1+2+3+4+5+6+7+8+9+10;
-            System.out.println(sum);
-
-        }
+    public void getSumNaturalNumbersFrom1ToNFor05() {
+        SumNaturalNumber calculator = new SumNaturalNumber();
+        int result = calculator.getSumNaturalNumbersFrom1ToN(5);
+        Assert.assertEquals(15, result);
     }
+
+    @Test
+    public void getSumNaturalNumbersFrom1ToNFor10() {
+        SumNaturalNumber calculator = new SumNaturalNumber();
+        int result = calculator.getSumNaturalNumbersFrom1ToN(10);
+        Assert.assertEquals(55, result);
+    }
+
+    @Test
+    public void getSumNaturalNumbersFrom1ToNFor03() {
+        SumNaturalNumber calculator = new SumNaturalNumber();
+        int result = calculator.getSumNaturalNumbersFrom1ToN(3);
+        Assert.assertEquals(6, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getSumNaturalNumbersFrom1ToNFor0() {
+        SumNaturalNumber calculator = new SumNaturalNumber();
+        calculator.getSumNaturalNumbersFrom1ToN(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getSumNaturalNumbersFrom1ToNForMinus3() {
+        SumNaturalNumber calculator = new SumNaturalNumber();
+        calculator.getSumNaturalNumbersFrom1ToN(-3);
+    }
+
 
 }
